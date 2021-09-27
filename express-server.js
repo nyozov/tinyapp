33af496ec6cf6f35dd0f82d2cbf86bf3c8dbb7e9
Nick Yozov <n_yozov@hotmail.com>
2021-09-27 21:06:15 +0000

    added random function, added urls_new

--- express-server.js
+++ express-server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.set("view engine", "ejs")
+
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -26,13 +30,29 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new")
+})
 app.get("/urls/:shortURL", (req, res) => {
-  console.log(urlDatabase[req.params.shortURL]);
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
   
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 
+function generateRandomString() {
+  let result = ''
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  for (let i = 0; i < 6; i++){
+    result += characters.charAt(Math.floor(Math.random() * characters.length))
+ 
+  }
+  return result
+}
 
