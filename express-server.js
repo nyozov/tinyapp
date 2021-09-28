3bf7329513875be097eeee5a30295faa6b212bb9
Nick Yozov <n_yozov@hotmail.com>
2021-09-28 21:50:58 +0000

    added update to url edits

--- express-server.js
+++ express-server.js
@@ -72,6 +72,14 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls")
 })
 
+app.post("/urls/:shortURL", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL
+  res.redirect("/urls")
+
+  
+
+})
+
 
 
 
