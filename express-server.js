7291d17e0be8389117f245703f58f1bb08e0a0c4
Nick Yozov <n_yozov@hotmail.com>
2021-09-29 16:59:43 +0000

    added logout

--- express-server.js
+++ express-server.js
@@ -4,6 +4,7 @@ const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 const cookieParser = require('cookie-parser')
+app.use(cookieParser())
 
 function generateRandomString() {
   let result = '';
@@ -37,10 +38,9 @@ app.listen(PORT, () => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { 
-    
-    
-    urls: urlDatabase};
+  const templateVars = {
+     username: req.cookies["username"], 
+     urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
@@ -52,6 +52,7 @@ app.get("/hello", (req, res) => {
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
@@ -83,9 +84,16 @@ app.post("/urls/:shortURL", (req, res) => {
 
 
 app.post("/login", (req, res)=> {
-  
-  res.cookie("username", req.body.username)
  
+  res.cookie("username", req.body.username)
+  res.redirect("/urls")
+  console.log(req.body.username)
+  
+
+})
+
+app.post("/logout", (req, res)=> {
+  res.clearCookie("username")
   res.redirect("/urls")
 
 })
