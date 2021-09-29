57f4c10ecbf4a915cb0516a7e88a1ac809341149
Nick Yozov <n_yozov@hotmail.com>
2021-09-29 01:36:14 +0000

    before changes

--- express-server.js
+++ express-server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+const cookieParser = require('cookie-parser')
 
 function generateRandomString() {
   let result = '';
@@ -36,7 +37,10 @@ app.listen(PORT, () => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase};
+  const templateVars = { 
+    
+    
+    urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
@@ -70,16 +74,21 @@ app.get("/u/:shortURL", (req, res) => {
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL]
   res.redirect("/urls")
-})
+});
 
 app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.longURL
   res.redirect("/urls")
+});
 
+
+app.post("/login", (req, res)=> {
   
+  res.cookie("username", req.body.username)
+ 
+  res.redirect("/urls")
 
 })
-
 
 
 
