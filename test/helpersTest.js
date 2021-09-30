65e0ef16b3223dbe8eb9ad6ec99d29c004d80374
Nick Yozov <n_yozov@hotmail.com>
2021-09-30 23:02:38 +0000

    added tests

---
+++ test/helpersTest.js
@@ -0,0 +1,29 @@
+const { assert } = require('chai');
+
+const { findUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('findUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = findUserByEmail("user@example.com", testUsers)
+    const expectedOutput = "userRandomID";
+    assert.equal(user.id, expectedOutput)
+  });
+  it('should return undefined if email not in database', function() {
+    const user = findUserByEmail("user3@example.com", testUsers)
+    const expectedOutput = undefined
+    assert.equal(user.id, expectedOutput)
+  })
+});
